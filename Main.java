be9bd66a362f8d03d6cd1e6de9e762c812fad720
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2021-11-28 21:19:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int currentAccount = 2_000_000_000;
+        int replenishmentAmount = 500_000_000;
+
+        int totalSum = currentAccount + replenishmentAmount;
+        System.out.println(totalSum);
+    }
+
+}
